8c2a99363ac5877c2e20e64ddacde19adab729e8
yasuos <y.sobajima@gmail.com>
2015-12-29 11:16:05 +0900

    安定的に動作するバージョン
    カウントダウンのUIを変更している。

--- src/app_main.c
+++ src/app_main.c
@@ -4,22 +4,17 @@
 #define PERSIST_KEY_WAKEUP_ID 42
 
 static Window *s_window = NULL;
+
 static TextLayer *s_text_layer = NULL;
+static TextLayer *s_count_layer = NULL;
 
 static ActionBarLayer *s_action_bar;
 static GBitmap *s_icon_start;
 static GBitmap *s_icon_stop;
 
 static WakeupId s_wakeup_id = 0;
-static char s_buffer[32];
+static char s_buffer[8];
 
-
-
-static void _text_layer_set_text( const char *text ){
-    if( s_text_layer != NULL ){
-        text_layer_set_text(s_text_layer, text );        
-    }
-}
 
 static void check_wakeup() {
     // Get the ID
@@ -31,24 +26,25 @@ static void check_wakeup() {
         int seconds_remaining = timestamp - time(NULL);
         
         // Show how many seconds to go
-        snprintf(s_buffer, sizeof(s_buffer), "Vibes works\nafter %d sec.", seconds_remaining);
-        _text_layer_set_text(s_buffer);
+        snprintf(s_buffer, sizeof(s_buffer), "%d", seconds_remaining);
+        if( s_count_layer != NULL ){
+            text_layer_set_text(s_count_layer, s_buffer);            
+        }
     }
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-    if( !app_worker_is_running()){
-        check_wakeup();
-    }
+    check_wakeup();
 }
 
 static void wakeup_start(){
+    wakeup_cancel_all();        
+    persist_delete(PERSIST_KEY_WAKEUP_ID);
+
     //Check the event is not already scheduled
     if (!wakeup_query(s_wakeup_id, NULL)) {
-        // Current time + 30 seconds
-        //time_t future_time = time(NULL) + 300;
-        time_t future_time = time(NULL) + 15;
-
+        time_t future_time = time(NULL) + 300;
+        //time_t future_time = time(NULL) + 15;
 
         // Schedule wakeup event and keep the WakeupId
         s_wakeup_id = wakeup_schedule(future_time, WAKEUP_REASON, false );
@@ -57,15 +53,16 @@ static void wakeup_start(){
 }
 
 static void wakeup_handler(WakeupId id, int32_t reason) {
-    vibes_short_pulse();
+    // バイブを2回鳴らす
+    vibes_double_pulse();
     
     // Delete the ID
-    persist_delete(PERSIST_KEY_WAKEUP_ID);
+    //persist_delete(PERSIST_KEY_WAKEUP_ID);
     
-    APP_LOG( APP_LOG_LEVEL_DEBUG, "waveup_woken" );
-
-    //
+    // もういちど実行する
     wakeup_start();
+
+    APP_LOG( APP_LOG_LEVEL_DEBUG, "waveup!" );
 }
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -74,17 +71,20 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
     if( id <= 0 ) {
         wakeup_start();
         
+        text_layer_set_text(s_text_layer, "vibes work to");
         action_bar_layer_set_icon(s_action_bar, BUTTON_ID_SELECT, s_icon_stop );
     } else {
-        _text_layer_set_text("Press Up button\nVibes works.");
+        text_layer_set_text(s_text_layer, "Press select.");
+        text_layer_set_text(s_count_layer, "000");
+
         action_bar_layer_set_icon(s_action_bar, BUTTON_ID_SELECT, s_icon_start );
 
+        wakeup_cancel_all();
         persist_delete(PERSIST_KEY_WAKEUP_ID);
-        wakeup_cancel_all();        
     }
 }
 
-
+/*
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
     wakeup_start();
 }
@@ -95,11 +95,12 @@ static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
     persist_delete(PERSIST_KEY_WAKEUP_ID);
     wakeup_cancel_all();
 }
+*/
 
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+  //window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  //window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
 static void window_load(Window *window) {
@@ -116,49 +117,56 @@ static void window_load(Window *window) {
     action_bar_layer_set_click_config_provider(s_action_bar,
                                              click_config_provider);
 
-    // Set the icons:
+    // icons load
     s_icon_start = gbitmap_create_with_resource(RESOURCE_ID_IMG_ACT_START);
     s_icon_stop = gbitmap_create_with_resource(RESOURCE_ID_IMG_ACT_STOP);
-/*
-    action_bar_layer_set_icon(s_action_bar, BUTTON_ID_UP, s_icon_start);
-    action_bar_layer_set_icon(s_action_bar, BUTTON_ID_DOWN, s_icon_stop );
-*/
-    action_bar_layer_set_icon(s_action_bar, BUTTON_ID_SELECT, s_icon_start );
     
-    //
-    s_text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w-ACTION_BAR_WIDTH , 40 } });
+    // メッセージフォント
+    s_text_layer = text_layer_create((GRect) { .origin = { 0, 52 }, .size = { bounds.size.w-ACTION_BAR_WIDTH , 20 } });
     text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
     layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+    
+    // カウンターフォント
+    s_count_layer = text_layer_create((GRect) { .origin = { 0, 68 }, .size = { bounds.size.w-ACTION_BAR_WIDTH , 46 } });
+    text_layer_set_font(s_count_layer, fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS));
+    text_layer_set_text_alignment(s_count_layer, GTextAlignmentCenter);
+    layer_add_child(window_layer, text_layer_get_layer(s_count_layer));
 
     // 動作しているのか確認する
     s_wakeup_id = persist_read_int(PERSIST_KEY_WAKEUP_ID);
     if( s_wakeup_id <= 0) {
-        _text_layer_set_text("Press Up button\nVibes works.");
+        text_layer_set_text(s_text_layer, "Press select.");
+        text_layer_set_text(s_count_layer, "000");
+        
+        action_bar_layer_set_icon(s_action_bar, BUTTON_ID_SELECT, s_icon_start );
+    } else {
+        text_layer_set_text(s_text_layer, "vibes work to");
+
+        action_bar_layer_set_icon(s_action_bar, BUTTON_ID_SELECT, s_icon_stop );        
     }
    
     APP_LOG( APP_LOG_LEVEL_DEBUG, "window_load" );
 }
 
-static void window_unload(Window *window) {
-    action_bar_layer_destroy( s_action_bar );
-    
+static void window_unload(Window *window) {    
     text_layer_destroy(s_text_layer);
+    text_layer_destroy(s_count_layer);
+
     gbitmap_destroy(s_icon_start);
     gbitmap_destroy(s_icon_stop);
+
+    action_bar_layer_destroy( s_action_bar );
 
     APP_LOG( APP_LOG_LEVEL_DEBUG, "window_unload" );
 }
 
 static void init(void) {
     s_window = window_create();
-    if( s_window == NULL ) {
-        return;
-    }
-    
     window_set_window_handlers(s_window, (WindowHandlers) {
         .load = window_load,
         .unload = window_unload,
     });
+
 /*
     // Subscribe to Wakeup API
     wakeup_service_subscribe(wakeup_handler);
@@ -167,6 +175,9 @@ static void init(void) {
     tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 */
     
+    // Subscribe to tick timer : 動かすな
+    tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+
     // Was this a wakeup launch?
     if (launch_reason() == APP_LAUNCH_WAKEUP) {
         // The app was started by a wakeup
@@ -177,23 +188,22 @@ static void init(void) {
         wakeup_get_launch_event(&id, &reason);
         wakeup_handler(id, reason);
     } else {
-        // Subscribe to Wakeup API
-        wakeup_service_subscribe(wakeup_handler);
+        //const bool animated = true;
+        // 動かすな
+        window_stack_push(s_window, true );
     
-        const bool animated = true;
-        window_stack_push(s_window, animated);
+        // Subscribe to Wakeup API：動かすな
+        wakeup_service_subscribe(wakeup_handler);
     }
 
-    // Subscribe to tick timer
-    tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
-
-    APP_LOG( APP_LOG_LEVEL_DEBUG, "window_init" );
+    APP_LOG( APP_LOG_LEVEL_DEBUG, "init" );
 }
 
 static void deinit(void) {
     tick_timer_service_unsubscribe();
     
     window_destroy(s_window);
+    
     APP_LOG( APP_LOG_LEVEL_DEBUG, "deinit" );
 }
 
